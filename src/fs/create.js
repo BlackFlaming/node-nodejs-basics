6e4f635c79ab966df57a0317c8df2038f18aacbb
BlackFlaming <yroslavg34@gmail.com>
2023-06-18 13:41:05 +0300

    Added file system opirations

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    try {
+        fs.writeFile(path.join(__dirname, "files", "fresh.txt"), "I am fresh and young", {
+            flag: 'wx'
+        }, (err) => {
+            if (err) throw "FS operation failed";
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
+    
 };
 
 await create();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from "fs";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const newPath = path.join(__dirname, "files", "properFilename.md");
+    const oldPath = path.join(__dirname, "files", "wrongFilename.txt");
+   
+    fs.exists(newPath, function (error) { 
+        if (!error) {
+            fs.rename(oldPath, newPath, function (err) {
+                if (err) {
+                    throw "FS operation failed"
+                }
+            });
+        }
+        else {
+            console.log("FS operation failed")
+        }
+    })
+    
 };
 
 await rename();
