562cde15291c01afe9eb70b138bd6aa6a9bf14c2
BlackFlaming <yroslavg34@gmail.com>
2023-06-19 17:18:53 +0300

    Added code for stream API

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const readStream = fs.createReadStream(path.join(__dirname, "files", "fileToRead.txt"));
+
+    readStream.on("data", function (chunk) {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import stream from "stream"
+
 const transform = async () => {
-    // Write your code here 
+    const reverser = new stream.Transform({
+        transform(chunk, encoding, callback) {
+            const chunkAsString = chunk.toString();
+            let stringToReturn = "";
+
+            for (let index = chunkAsString.length-1; index > -1; index--) {
+                stringToReturn += chunkAsString[index];
+            }
+            this.push(stringToReturn);
+            callback();
+        }
+    })
+    process.stdin.pipe(reverser).pipe(process.stdout);
 };
 
 await transform();
